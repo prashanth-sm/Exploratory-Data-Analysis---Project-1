f3dc0e6c5f50b9dc751a9d47d5d80352d606bdca
prashanth-sm <gowriprashanth.s.m@gmail.com>
2016-03-21 01:52:41 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -21,7 +21,7 @@ rm(full.data)
 datetime <- paste(as.Date(required.data$Date), required.data$Time)
 required.data$Datetime <- as.POSIXct(datetime)
 
-## Plot 2 
+## Plot 3
 with(required.data, {plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="") 
 lines(Sub_metering_2~Datetime,col='Red') 
 lines(Sub_metering_3~Datetime,col='Blue') 
